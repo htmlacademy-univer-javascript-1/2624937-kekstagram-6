5f876abc51738fb92d3100907658d98b86b70ce6
dislert <konstantintropin93@gmail.com>
2025-10-13 15:06:36 +0500

    5.9

--- js/functions.js
+++ js/functions.js
@@ -6,18 +6,18 @@ strLength('');
 palindrommCheck('');
 
 const CheckTimeMeeting = (workStartTime, workEndTime, meetingStartTime, meetingDuration) => {
-    const toMinutes = (timeStr) =>  {
-        const [h, m] = timeStr.split(':').map(Number);
-        return h * 60 + m;
-    }
+  const toMinutes = (timeStr) =>  {
+    const [h, m] = timeStr.split(':').map(Number);
+    return h * 60 + m;
+  };
 
-    const startWorkMin = toMinutes(workStartTime);
-    const endWorkMin = toMinutes(workEndTime);
-    const startMeetingMin = toMinutes(meetingStartTime);
-    const endMeetingMin = startMeetingMin + meetingDuration;
+  const startWorkMin = toMinutes(workStartTime);
+  const endWorkMin = toMinutes(workEndTime);
+  const startMeetingMin = toMinutes(meetingStartTime);
+  const endMeetingMin = startMeetingMin + meetingDuration;
 
-    return startMeetingMin >= startWorkMin && endMeetingMin <= endWorkMin;
-}
+  return startMeetingMin >= startWorkMin && endMeetingMin <= endWorkMin;
+};
 
 CheckTimeMeeting('08:00', '17:30', '14:00', 90); // true
 CheckTimeMeeting('8:0', '10:0', '8:0', 120);     // true
