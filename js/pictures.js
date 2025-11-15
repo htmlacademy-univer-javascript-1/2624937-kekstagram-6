61734779568cfb020aed8739a7f0095fb5e10a04
dislert <konstantintropin93@gmail.com>
2025-11-15 16:50:48 +0300

    8.10

--- js/pictures.js
+++ js/pictures.js
@@ -1,22 +1,36 @@
-const createThumbnailElement = (photoData) => {
-  const template = document.querySelector('#picture');
-  const thumbnail = template.content.querySelector('.picture').cloneNode(true);
-  const imgElement = thumbnail.querySelector('.picture__img');
-  imgElement.src = photoData.url;
-  imgElement.alt = photoData.description;
-  thumbnail.querySelector('.picture__likes').textContent = photoData.likes;
-  thumbnail.querySelector('.picture__comments').textContent = photoData.comments.length;
-  thumbnail.dataset.thumbnailId = photoData.id;
+import { exportPhotos } from './data.js';
+import { openBigPicture } from './fullscreen.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+
+const createThumbnail = (photo) => {
+  const thumbnail = pictureTemplate.cloneNode(true);
+
+  const image = thumbnail.querySelector('.picture__img');
+  image.src = photo.url;
+  image.alt = photo.description;
+
+  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(photo);
+  });
+
   return thumbnail;
 };
 
-const renderThumbnails = (photos) => {
-  const picturesContainer = document.querySelector('.pictures');
+const renderThumbnails = () => {
   const fragment = document.createDocumentFragment();
-  photos.forEach((photo) => {
-    fragment.appendChild(createThumbnailElement(photo));
+
+  exportPhotos.forEach((photo) => {
+    const thumbnail = createThumbnail(photo);
+    fragment.appendChild(thumbnail);
   });
+
   picturesContainer.appendChild(fragment);
 };
 
-export {renderThumbnails};
+export { renderThumbnails };
